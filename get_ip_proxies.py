00963ce49672dd000f6719a14bb4fbd5f7e81346
wangyang <missgoon@163.com>
2016-04-05 15:03:01 +0800

    修改过去ip代理的方法

--- get_ip_proxies.py
+++ get_ip_proxies.py
@@ -3,17 +3,32 @@ import requests
 import lxml.html
 import redis
 
-url="http://www.youdaili.net/Daili/http/4304.html"
-html=lxml.html.fromstring(requests.get(url).content)
+urls=[
+  "http://www.youdaili.net/Daili/http/4309.html",
+  "http://www.youdaili.net/Daili/http/4309_2.html",
+  "http://www.youdaili.net/Daili/http/4309_3.html",
+]
 r = redis.Redis(host="139.129.45.40",port=6379,db=0)
-with open("ip_proxies","w") as file:
-  for item in html.xpath("//span[@style='font-size:14px;']/text()"):
-    try:
-      item=item.strip().split("@")[0]
-      file.write(item+"\n")
-      r.lpush("ip_proxies",item)
-      print(r.llen("ip_proxies"))
-    except Exception,e:
-      print(e)
+r.flushall()
+with open("ip_proxies","a") as file:
+  def handle_item(item):
+	  try:
+	    item=item.strip().split("@")[0]
+	    if not len(item)>0: raise
+	    file.write(item+"\n")
+	    r.lpush("ip_proxies",item)
+	    print("%s\t%d"%(item,r.llen("ip_proxies")))
+	  except Exception,e:
+	    print(e)
+  for url in urls:
+    html=lxml.html.fromstring(requests.get(url).content)
+    if len(html.xpath("//span[@style='font-size:14px;']"))>=1:
+      for item in html.xpath("//span[@style='font-size:14px;']/text()"):
+        handle_item(item)
+    elif len(html.xpath("//div[@class='cont_font']/p/text()"))>0:
+      for item in html.xpath("//div[@class='cont_font']/p/text()"):
+        handle_item(item)
+
+
 
 
