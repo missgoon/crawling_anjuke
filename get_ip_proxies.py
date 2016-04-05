1bb273531710a5c44723b11d7d43aa4869c9ea01
wangyang <missgoon@163.com>
2016-04-05 16:20:22 +0800

    update ip proxy

--- get_ip_proxies.py
+++ get_ip_proxies.py
@@ -8,18 +8,23 @@ urls=[
   "http://www.youdaili.net/Daili/http/4309_2.html",
   "http://www.youdaili.net/Daili/http/4309_3.html",
 ]
+test_url="http://chaoyang.anjuke.com/"
 r = redis.Redis(host="139.129.45.40",port=6379,db=0)
 r.flushall()
 with open("ip_proxies","a") as file:
   def handle_item(item):
-	  try:
-	    item=item.strip().split("@")[0]
-	    if not len(item)>0: raise
-	    file.write(item+"\n")
-	    r.lpush("ip_proxies",item)
-	    print("%s\t%d"%(item,r.llen("ip_proxies")))
-	  except Exception,e:
-	    print(e)
+    try:
+      item=item.strip().split("@")[0]
+      if not len(item)>0: raise
+      proxies = {'http':"http://"+item}
+      response=requests.get(test_url,proxies=proxies,timeout=3)
+      print(proxies,response)
+      if response.status_code!=200: raise
+      file.write(item+"\n")
+      r.lpush("ip_proxies",item)
+      print("%s\t%d"%(item,r.llen("ip_proxies")))
+    except Exception,e:
+      print(e)
   for url in urls:
     html=lxml.html.fromstring(requests.get(url).content)
     if len(html.xpath("//span[@style='font-size:14px;']"))>=1:
