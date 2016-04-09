0bd49c66be25aa43dd9f785aac6373718ff696b2
missgoon <missgoon@163.com>
2016-04-10 03:07:50 +0800

    update

--- anjuke/pipelines.py
+++ anjuke/pipelines.py
@@ -49,6 +49,7 @@ class AnjukeHouseItemPipeline(object):
     client=MongoClient("139.129.45.40",27017)
     db=client.anjuke
     self.houses=db.houses
+    self.keys=["status","price","property_type","developer","address","area_position","telephone","min_down_payment","opening_date","house_type","sales_office_add","building_types","year_of_property","fitment","plot_ratio","greening_rate","floor_condition","works_programme","managefee","property_management","freeway_viaduct"]
 
   def process_item(self,item,spider):
     if item.get("db_type","false")=="houses" and len(item.get("db_id",""))!=0:
@@ -56,6 +57,12 @@ class AnjukeHouseItemPipeline(object):
       if db_item.count()==0:
         item["date"]=DateTime.utcnow()
         self.houses.insert_one(dict(item))
+      # elif len(dict(item))>len(dict(db_item)):
+      #   self.houses.update({"db_id":item["db_id"]},{"$set":dict(item)})
       else:
-        self.houses.update({"db_id":item["db_id"]},{"$set":dict(item)})
+        db_item=db_item[0]
+        for key in self.keys:
+          if item.get(key,None) and (item.get(key,"null")!="null" or db_item.get(key,"null")=="null"): self.houses.update_one({"db_id":db_item["db_id"]},{"$set":{key:item.get(key,"null")}})
+        logging.info("######process_item:")
+        logging.info(self.houses.find_one(db_item["_id"]))
     return item
